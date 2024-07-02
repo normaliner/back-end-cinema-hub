e907c184f054a4639dc4624c6d202297136c8d08
normaliner <prokopenko.viktor110701@mail.ru>
2024-07-02 19:08:49 +0300

    Create FileController

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+import { FileResponse } from './file.interface';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+	async saveFiles(
+		files: Express.Multer.File[],
+		folder: string = 'default'
+	): Promise<FileResponse[]> {
+		const uploadFolder = `${path}/uploads/${folder}`;
+		
+		await ensureDir(uploadFolder);
+		
+		const response: FileResponse[] = await Promise.all(
+			files.map(async file => {
+				await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
+				return {
+					url: `/uploads/${folder}/${file.originalname}`,
+					name: file.originalname
+				};
+			})
+		);
+
+		return response;
+	}
+}
